82d12159330d6b75ccd1860f449025f9e4bf9594
j3rin <jerrymic2255@gmail.com>
2023-01-23 22:40:54 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Buttons} from "./components/Buttons"
+import { Typo } from "./components/Typography";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typo/>
+      <Buttons/>
+    </div>
+  );
+}
+
+export default App;
